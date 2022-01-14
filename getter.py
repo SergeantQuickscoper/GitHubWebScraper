3fd5507f3b4ea35e959dadb4a301dad5603e7caa
SergeantQuickscoper <donisepic30@gmail.com>
2022-01-14 22:29:19 +0300

    Can find names of github users now

--- getter.py
+++ getter.py
@@ -1,14 +1,19 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
+
+    
+
 username = input("Enter a valid GitHub User: ")
 website = "https://github.com/"
 finalURL = website + username
 
 r = requests.get(finalURL)
-
 soup = bs(r.content, "html.parser")
-
 profile_image = soup.find('img', class_ = "avatar avatar-user width-full border color-bg-default")["src"]
+name = soup.find('span', class_ ="p-name vcard-fullname d-block overflow-hidden")
+name = name.string
 
-print(profile_image)
+print("Name: " + name)
+print("Profile Picture: " + profile_image)
+
