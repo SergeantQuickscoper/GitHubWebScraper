1ea09443a2436668a762b55d81140c82f8f60543
SergeantQuickscoper <donisepic30@gmail.com>
2022-01-15 20:53:16 +0300

    small change not significant

--- getter.py
+++ getter.py
@@ -12,7 +12,8 @@ r = requests.get(finalURL)
 soup = bs(r.content, "html.parser")
 profile_image = soup.find('img', class_ = "avatar avatar-user width-full border color-bg-default")["src"]
 name = soup.find('span', class_ ="p-name vcard-fullname d-block overflow-hidden")
-name = name.string
+name = name.get_text()
+
 
 print("Name: " + name)
 print("Profile Picture: " + profile_image)
