74c2e62b8ed045b0b89384939b6a0b5af35ec80b
SergeantQuickscoper <donisepic30@gmail.com>
2022-01-16 21:20:46 +0300

    followers and following implented

--- getter.py
+++ getter.py
@@ -1,20 +1,27 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
-
-    
-
 username = input("Enter a valid GitHub User: ")
 website = "https://github.com/"
 finalURL = website + username
 
 r = requests.get(finalURL)
-soup = bs(r.content, "html.parser")
-profile_image = soup.find('img', class_ = "avatar avatar-user width-full border color-bg-default")["src"]
-name = soup.find('span', class_ ="p-name vcard-fullname d-block overflow-hidden")
-name = name.get_text()
+r.encoding = r.apparent_encoding
+content = r.text
+r.raise_for_status()
 
+soup = bs(content, "html.parser")
+
+profile_image = soup.find('img', class_ = "avatar avatar-user width-full border color-bg-default")["src"]
+
+name = soup.find('span', {"itemprop" : "name"}).get_text()
+
+followersArray = soup.find_all("span", class_ = "text-bold color-fg-default")
+followers, following = followersArray
+followers = followers.string
+following = following.string
 
 print("Name: " + name)
 print("Profile Picture: " + profile_image)
-
+print("Followers: " + followers)
+print("Following: " + following)
